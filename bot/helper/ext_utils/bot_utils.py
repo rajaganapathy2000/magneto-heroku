4c5f299c80ee86c576a2f811e0fb66eb186a1093
Avijneyam <avijneyam@gmail.com>
2020-10-28 13:38:17 +0530

    ok

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -92,20 +92,20 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>Filename :</b> <code>{download.name()}</code>"
-            msg += f"\n<b>Status :</b> <i>{download.status()}</i>"
+            msg += f"<i>{download.status()}</i>"
+            msg += f"\n<b>Name :</b> <code>{download.name()}</code>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
-                       f"\n<b>Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
-                       f"\n<b>Speed :</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
+                msg += f"\n<code>{get_progress_bar_string(download)} // {download.progress()}</code>" \
+                       f"\n<b>DL :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()} " \
+                       f"// {download.speed()} \n{download.eta()} "
+            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                msg += f"\n|| <code>/kill {download.gid()}</code>"
                 # if hasattr(download, 'is_torrent'):
                 try:
-                    msg += f"\n<b>Info :- Seeders:</b> {download.aria_download().num_seeders}" \
-                        f" & <b>Peers :</b> {download.aria_download().connections}"
+                    msg += f"\n<b>Info :- seeders:</b> {download.aria_download().num_seeders}" \
+                        f" // <b>peers :</b> {download.aria_download().connections}"
                 except:
                     pass
-            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>GID</b>: <code>{download.gid()}</code>"
             msg += "\n\n"
         return msg
 
