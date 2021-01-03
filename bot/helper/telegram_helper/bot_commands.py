2b583c412b2fbb578f2c8f76fef0669fe2ec790a
GokuMUI7 <39430301+GokuMUI7@users.noreply.github.com>
2021-01-03 16:51:19 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,7 +1,7 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'darpan'
+        self.MirrorCommand = 'mirror'
         self.UnzipMirrorCommand = 'unzip'
         self.TarMirrorCommand = 'tar'
         self.CancelMirror = 'kill'
